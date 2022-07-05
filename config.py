3c599fddb7859b53865db1de6f6d8437f8206f3b
natcebron <104511703+natcebron@users.noreply.github.com>
2022-07-05 15:54:27 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+"""
+
+Config file for Streamlit App
+
+"""
+
+from member import Member
+
+
+TITLE = "My Awesome App"
+
+TEAM_MEMBERS = [
+
+    Member(name="Nathan CEBRON",
+        linkedin_url="https://www.linkedin.com/in/nathan-cebron-9992a1a4/")
+]
+
+PROMOTION = "Promotion Bootcamp Data Scientist - Mai 2022"
+
+MENTOR = "Gaspard GRIMM"
