3510d568e68edcd64ba16b4d98507142ed2caf55
natcebron <104511703+natcebron@users.noreply.github.com>
2022-08-03 15:19:07 +0200

    Update jour_ATP.py

--- jour_ATP.py
+++ jour_ATP.py
@@ -68,6 +68,7 @@ def app():
     odds_5 = pd.concat([odds_1,odds_4],axis=1)
     #st.dataframe(odds_5)
     odds_5 = odds_5.drop(['resulting_unit','periods','draw','period_results'],axis=1)
+    st.write(odds_5.columns)
     odds_5.columns = ['league_id', 'league_name', 'starts', 'home_player','away_player','home_odds','away_odds']
     #odds_5.columns = ['league_id', 'league_name', 'starts', 'home_player','away_player','t','away_odds','home_odds']
     #odds_5 = odds_5[['league_id', 'league_name', 'starts', 'home_player','away_player','t','home_odds','away_odds']]
